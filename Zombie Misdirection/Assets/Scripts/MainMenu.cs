fa057fdf69e206bb12f861171a69465a7be7dee0
Liahm <ariete-lee@hotmail.com>
2018-07-15 22:55:16 -0400

    Adding the main menu and functionalities

--- Zombie Misdirection/Assets/Scripts/MainMenu.cs
+++ Zombie Misdirection/Assets/Scripts/MainMenu.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour 
 {
@@ -10,21 +11,58 @@ public class MainMenu : MonoBehaviour
 	public bool VERBOSE = false;
 
 //---------------------------------------------------------------------------FIELDS:
+
+	public GameObject CanvasObject;
+
+	private bool x;
+
+//-------------------------------------------------------------------MONO METHODS:
 	
-//---------------------------------------------------------------------MONO METHODS:
-
-	void Start() 
-	{
-
-	}
-		
 	void Update()
-    {
-
-    }
+	{
+		if(Input.GetKeyUp(KeyCode.R))
+		{
+			SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+		}
+		if(Input.GetKeyUp(KeyCode.Escape))
+		{
+			if(!x)	
+			{
+				CanvasObject.SetActive(true);
+				x = true;
+			}
+			else
+			{
+				CanvasObject.SetActive(false);
+				x = false;
+			}
+		}
+		if(Input.GetKeyDown(KeyCode.N))
+		{
+			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+		}
+		else if(Input.GetKeyDown(KeyCode.M))
+		{
+			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+		}
+	}
 
 //--------------------------------------------------------------------------METHODS:
 	//Main menu UI buttons
+	public void StartGame()
+	{
+		if(SceneManager.GetActiveScene().name == "GameOver")
+		{
+			SceneManager.LoadScene("Tutorial 1");	
+		}
+		else
+			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);		
+	}
+
+	public void Quit()
+	{
+		Application.Quit();
+	}
 //--------------------------------------------------------------------------HELPERS:
 	
 }
