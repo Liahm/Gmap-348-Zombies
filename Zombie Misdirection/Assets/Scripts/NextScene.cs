022ed34aefd1bf51458d23f7dc12d7b75428e562
Liahm <ariete-lee@hotmail.com>
2018-07-20 19:35:32 -0400

    Added functionality for multiple scientist

--- Zombie Misdirection/Assets/Scripts/NextScene.cs
+++ Zombie Misdirection/Assets/Scripts/NextScene.cs
@@ -12,6 +12,9 @@ public class NextScene : MonoBehaviour
 
 //---------------------------------------------------------------------------FIELDS:
 	public float Delay;
+	public int ScientistCount = 1;
+
+	private int currentScientistCount;
 //---------------------------------------------------------------------MONO METHODS:
 
 
@@ -21,18 +24,27 @@ public class NextScene : MonoBehaviour
 	/// Literally load next scene on call.
 	/// Be it on trigger or by UI or by button (debugging)
 	///</TODO>
-
+	void Start()
+	{
+		currentScientistCount = 0;
+	}
 	void OnTriggerEnter(Collider col)
 	{
 		if(col.tag == "Scientist")
 		{
-			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+			currentScientistCount++;
 		}
 		else if (col.tag == "Human")
 		{
 			Destroy(col.gameObject, Delay);
 		}
+
+		if(currentScientistCount == ScientistCount)
+		{
+			SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+		}
 	}
+
 //--------------------------------------------------------------------------HELPERS:
 	
 }
